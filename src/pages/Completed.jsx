f4ed6f2d5201d251c05d26aaee8794d8a47395e1
Davidokih <davidyole023@gmail.com>
2024-05-18 15:37:51 +0100

    update

--- src/pages/Completed.jsx
+++ src/pages/Completed.jsx
@@ -5,9 +5,11 @@ const Completed = () => {
         <div className="rounded-md bg-white font-medium text-sm mw-[350px] flex justify-center items-center shadow">
               <div className="text-indigo-950 mt-7 flex flex-col items-center mb-7 w-[75%]">
                   <div className="flex justify-center text-xl rounded-full items-center shadow w-10 h-10 mb-5 text-green-400"><FaCheck /></div>
-                  <h1 className="text-2xl font-bold mb-4">Purchase Completed</h1>
+                  <div>
+                      <h1 className="text-2xl font-bold mb-4">Purchase Completed</h1>
                   <div className="text-sm font-medium">Please check email for other concerning this transaction</div>
                   <div className="mt-8 text-orange-400 font-semibold underline">Return Home</div>
+                  </div>
               </div>          
         </div>
     </>
