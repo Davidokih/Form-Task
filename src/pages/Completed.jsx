d9a20d26841e00ca737f5e0feb826eb088f7960a
Davidokih <davidyole023@gmail.com>
2024-05-22 14:11:37 +0100

    update

--- src/pages/Completed.jsx
+++ src/pages/Completed.jsx
@@ -1,30 +1,14 @@
-import PropTypes from "prop-types"
-import { useState } from "react";
-
-const Completed = ({ name, notes, state,covid19Site }) => {
-  
-   const [isExpanded, setIsExpanded] = useState(false);
-
-  const toggleExpansion = () => {
-    setIsExpanded(!isExpanded);
-  };
-
-  // Determine if the text needs to be truncated
-  const shouldTruncate = notes.length > 100;
-
-  // Truncate the text if necessary
-  const displayedText = shouldTruncate && !isExpanded
-    ? `${notes.substring(0, 100)}...`
-    : notes;
+import { FaCheck } from "react-icons/fa";
+const Completed = () => {
   return (
     <>
         <div className="rounded-md bg-white font-medium text-sm mw-[350px] flex justify-center items-center shadow">
-              <div className="text-indigo-950 mt-7 mb-7 w-[75%]">
-          <div className="flex justify-center text-xl rounded-full items-center shadow w-10 h-10 mb-5 text-green-400">{ state}</div>
+              <div className="text-indigo-950 mt-7 flex flex-col items-center mb-7 w-[75%]">
+                  <div className="flex justify-center text-xl rounded-full items-center shadow w-10 h-10 mb-5 text-green-400"><FaCheck /></div>
                   <div>
-            <h1 className="text-2xl font-bold mb-4">{ name}</h1>
-            <div className="text-sm font-medium">{displayedText }</div>
-                 <a href={covid19Site}> <div className="mt-8 text-orange-400 font-semibold underline">For more Info</div></a>
+                      <h1 className="text-2xl font-bold mb-4">Purchase Completed</h1>
+                  <div className="text-sm font-medium">Please check email for other concerning this transaction</div>
+                  <div className="mt-8 text-orange-400 font-semibold underline">Return Home</div>
                   </div>
               </div>          
         </div>
@@ -32,10 +16,4 @@ const Completed = ({ name, notes, state,covid19Site }) => {
   )
 }
 
-Completed.propTypes = {
-  name: PropTypes.string.isRequired,
-  notes: PropTypes.string.isRequired,
-  state: PropTypes.string.isRequired,
-  covid19Site: PropTypes.string.isRequired,
-}
 export default Completed
